ef2db5a4fe9fc14b3f5e7c576cb43a8d36a634f2
melissa kaitesi <mulungimelissa@gmail.com>
2019-06-17 11:03:13 +0300

    Number-Guessing-Game

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //Number Guessing Game
-var i= Math.floor(Math.random()*100) +.1;//Generates random number and stores it i
+var i= Math.floor(Math.random()*100) +1;//Generates random number and stores it i
 
 var j=i%2;// gets the modulus and store the result in j
 if (j==0)//check to see if result in step three is zore or not
 	   alert( "The number is Even");
 else
-		alert("The number is add odd");
+		alert("The number is odd");
 for (count=1;count<=10;count++)
 {
 let num = prompt ("please enter a number:")//Captures user input
 
-		if (num<i){
+		if (num>i){
 			alert("Your number is higher. Make another guess");
 		}
 		if (num<i){
